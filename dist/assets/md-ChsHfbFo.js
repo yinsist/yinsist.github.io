4eaae627806527e1dfe3666c4921cdebf56cf61a
Ywinh <jianhui30107@163.com>
2026-01-16 17:23:59 +0800

    add dist

---
+++ dist/assets/md-ChsHfbFo.js
@@ -0,0 +1,7 @@
+import{b as a,o as n,w as m,g as o,i as p,v as i,x as l,T as s}from"./modules/vue-3MX-AAUY.js";import{I as d}from"./slidev/default-N-ojdLkz.js";import{u,f as g}from"./slidev/context-CCvA-To0.js";import"./index-CPDaYc2D.js";import"./modules/shiki-DyOxNjFt.js";const f="/assets/gem5_port-B4EoZ1B4.png",C={__name:"slides.md__slidev_5",setup(_){const{$clicksContext:e,$frontmatter:r}=u();return e.setup(),(c,t)=>(n(),a(d,i(l(s(g)(s(r),4))),{default:m(()=>[t[0]||(t[0]=o("h1",null,"Gem5通信",-1)),t[1]||(t[1]=o("p",null,"分为 normal 和需要 retry 的情况",-1)),p(` <div grid="~ cols-2 gap-2" m="t-2">
+
+![](./images/gem5-port1.png)
+
+![](./images/gem5-port2.png)
+
+</div> `),t[2]||(t[2]=o("p",null,[o("img",{src:f,alt:"",width:"550",style:{display:"block",margin:"auto"}})],-1))]),_:1},16))}};export{C as default};
